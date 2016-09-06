fab50d7cf7ee3cc8632831e065094f68478d286d
cponeill <caseyoneill78@hotmail.com>
2016-09-06 18:42:06 -0400

    15 lessons.

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,27 +17,15 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    if a == 2 and b == 2 and c == 2:
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError(AttributeError('Negative edge is not allowed'))
+    if a + b + c <= 2 * max(a, b, c):
+        raise TriangleError(AttributeError('largest side should be smaller than sum of 2 other sides'))
+    if a == b == c:
         return 'equilateral'
-    if a == 10 and b == 10 and c == 10:
-        return 'equilateral'
-
-    if a == 3 and b and c == 4:
+    elif a == b or b == c or c == a:
         return 'isosceles'
-    if a and c == 4 and b == 3:
-        return 'isosceles'
-    if a and b == 4 and c == 3:
-        return 'isosceles'
-    if a == 10 and b == 10 and c == 2:
-        return 'isosceles'
-
-    if a == 3 and b == 4 and c == 5:
-        return 'scalene'
-    if a == 10 and b == 11 and c == 12:
-       return 'scalene'
-    if a == 5 and b == 4 and c == 2:
-       return 'scalene'    
-    
+    return 'scalene'    
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
     pass
