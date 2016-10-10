4e5fc091444322c187388937faad59bf6bc00364
cponeill <caseyoneill78@hotmail.com>
2016-10-10 11:33:16 -0400

    more changes.

--- python2/koans/about_classes.py
+++ python2/koans/about_classes.py
@@ -87,7 +87,7 @@ class AboutClasses(Koan):
         fido = self.Dog4()
 
         fido.name = "Fido"
-        self.assertEqual(__, fido.name)
+        self.assertEqual('Fido', fido.name)
 
     # ------------------------------------------------------------------
 
@@ -101,10 +101,10 @@ class AboutClasses(Koan):
 
     def test_init_provides_initial_values_for_instance_variables(self):
         fido = self.Dog5("Fido")
-        self.assertEqual(__, fido.name)
+        self.assertEqual('Fido', fido.name)
 
     def test_args_must_match_init(self):
-        self.assertRaises(___, self.Dog5)  # Evaluates self.Dog5()
+        self.assertRaises(TypeError, self.Dog5)  # Evaluates self.Dog5()
 
         # THINK ABOUT IT:
         # Why is this so?
@@ -113,7 +113,7 @@ class AboutClasses(Koan):
         fido = self.Dog5("Fido")
         rover = self.Dog5("Rover")
 
-        self.assertEqual(____, rover.name == fido.name)
+        self.assertEqual('False', rover.name == fido.name)
 
     # ------------------------------------------------------------------
 
